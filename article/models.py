d3959e222793abaaf50b4899eb61c39fc45aeaa4
gabigab117 <gabrieltrouve5@gmail.com>
2024-06-06 00:55:44 +0200

    add comment Model and comment to article page

--- article/models.py
+++ article/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
 from wagtail.fields import RichTextField, StreamField
 from wagtail.models import Page, ParentalKey
 from wagtail.admin.panels import FieldPanel
@@ -7,6 +10,9 @@ from wagtail.images.blocks import ImageChooserBlock
 from wagtail.search import index
 from modelcluster.contrib.taggit import ClusterTaggableManager
 from taggit.models import TaggedItemBase
+from wagtail.snippets.models import register_snippet
+
+User = get_user_model()
 
 
 class ArticleIndexPage(Page):
@@ -53,6 +59,23 @@ class ArticlePage(Page):
         index.SearchField("intro")
     ]
 
+    def serve(self, request, *args, **kwargs):
+        from .forms import CommentForm
+
+        comments = self.comments.all()
+
+        if request.method == "POST":
+            form = CommentForm(request.POST)
+            if form.is_valid():
+                comment = form.save(commit=False)
+                comment.user, comment.page = request.user, self
+                comment.save()
+                return HttpResponseRedirect(request.path)
+        else:
+            form = CommentForm()
+        return render(request, "article/article_page.html", context={"page": self, "form": form,
+                                                                     "comments": comments})
+
 
 class ArticleTagIndexPage(Page):
     def get_context(self, request, *args, **kwargs):
@@ -61,3 +84,19 @@ class ArticleTagIndexPage(Page):
         context = super().get_context(request, *args, **kwargs)
         context["articlepages"] = articlepages
         return context
+
+
+@register_snippet
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Utilisateur")
+    page = models.ForeignKey("article.ArticlePage", on_delete=models.CASCADE, related_name="comments")
+    text = models.TextField(verbose_name="Commentaire")
+
+    def __str__(self):
+        return f"{self.user} - {self.page.title}"
+
+    panels = [
+        FieldPanel("user"),
+        FieldPanel("text"),
+        FieldPanel("page")
+    ]
